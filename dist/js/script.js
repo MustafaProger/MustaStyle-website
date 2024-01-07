d2fb91d0dab57904a9f5a5fe5a3230740ca269f1
DIOR <diertojiev@yandex.ru>
2024-01-07 23:59:00 +0300

    Добавлена адаптация на третий блок

--- dist/js/script.js
+++ dist/js/script.js
@@ -17,6 +17,7 @@ $(document).ready(function () {
     });
 
     //появление скрытого navbar при нажатии на бургер
+    //анимация на список ul
     $('.burger__menu').on('click', function () {
         $('.burger__menu').toggleClass('active');
         $('.navbar').toggleClass('active');
@@ -25,11 +26,38 @@ $(document).ready(function () {
     });
 
     //работа с slick-слайдер
-    var slider = tns({
-        container: '.my-slider',
-        items: 1,
-        slideBy: 'page',
-        // autoplay: true
-        centerPadding: 1000
-      });
+    $('.carousel__inner').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrows/arrowLeft.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrows/arrowRight.svg"></button>',
+        responsive: [{
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 618,
+                settings: {
+                    dots: true,
+                    arrows: false,
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+
+                }
+            }
+        ]
+    });
 })
